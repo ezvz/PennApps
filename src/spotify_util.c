135e85451d188af06759dc5ee9f2badb9c53e020
Mishal Awadah <a.mamish@gmail.com>
2012-09-15 17:25:18 -0400

    spotify util additions

--- src/spotify_util.c
+++ src/spotify_util.c
@@ -35,6 +35,8 @@ static pthread_cond_t  g_cond  = PTHREAD_COND_INITIALIZER;
 
 #define PF_USER_AGENT "pandorify"
 
+sp_session *g_session;
+
 // Convenience macros:
 #define G_MUTEX_SYNCHRONIZE(code) do {\
   pthread_mutex_lock(&(g_mutex));\
@@ -63,7 +65,15 @@ static sp_session_callbacks callbacks = {
     .notify_main_thread = pf_notify_main_thread,
 };
 
-int spotify_init(const char *username, const char* password) {
+/** 
+ * Initialize and login to create a session.
+ * 
+ * @param username 
+ * @param password 
+ * 
+ * @return 0 on success, error otherwise
+ */
+int pf_spotify_init(const char *username, const char* password) {
     sp_session *session;
     sp_session_config config;
     sp_error error;
@@ -75,13 +85,33 @@ int spotify_init(const char *username, const char* password) {
     config.application_key_size = g_appkey_size;
     config.user_agent = PF_USER_AGENT;
     config.callbacks = &callbacks;
+
+    error = sp_session_create(&config, &session);
+    ASSERT_SP_ERROR_OK(error);
+    MESSAGE("Created session successfully.");
+    
+    G_WAIT(
+           MESSAGE("Logging in...");
+           sp_session_login(session, username, password, 0, NULL);
+           MESSAGE("(waiting...)");
+           );
+
+    ASSERT_SP_ERROR_OK(error);
+    INFO("Logged in: %s", (
+                           sp_session_connectionstate(session) == SP_CONNECTION_STATE_LOGGED_IN ? "yes" : "no"
+                           ));
+    g_session = session;
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     MESSAGE("Running spotify_util");
-    
-    
+    if (argc < 2) {
+        FAIL("Not enough arguments");
+    }
+    printf("%d\n", argc);
+    pf_spotify_init(argv[1], argv[2]);
     
     return 0;
 }
