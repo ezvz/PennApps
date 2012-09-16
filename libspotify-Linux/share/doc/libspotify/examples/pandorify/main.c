11f4ec0a0b7af9c04fdc51325dfc8d495d5a9cae
Ubuntu <ubuntu@domU-12-31-39-00-DD-60.compute-1.internal>
2012-09-16 03:59:32 +0000

    got server working

---
+++ libspotify-Linux/share/doc/libspotify/examples/pandorify/main.c
@@ -0,0 +1,216 @@
+/**
+ * Copyright (c) 2006-2010 Spotify Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#define USER_AGENT "pandorify"
+
+/* A simple stub that logs in and out */
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <libgen.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <libspotify/api.h>
+
+const uint8_t g_appkey[] = {
+	0x01, 0x2B, 0x3B, 0xC6, 0xE1, 0x95, 0xBE, 0xF1, 0x28, 0x03, 0x57, 0x63, 0xC9, 0xC2, 0x10, 0x53,
+	0xF3, 0x69, 0x4A, 0x40, 0xB1, 0x4F, 0x99, 0x4E, 0xFC, 0xDB, 0x1A, 0x4B, 0xD8, 0x9B, 0x2E, 0x56,
+	0xB4, 0xF1, 0xD1, 0x42, 0xA0, 0x3A, 0x36, 0xE2, 0x16, 0xDC, 0xF9, 0xC7, 0x50, 0x54, 0x62, 0x7F,
+	0x8A, 0xB5, 0x02, 0x96, 0xC4, 0xF5, 0x1E, 0xF6, 0x84, 0xA6, 0xAA, 0xBA, 0xE7, 0x4A, 0xB2, 0xB0,
+	0xC2, 0xE1, 0x19, 0xD8, 0xCC, 0xDE, 0xC1, 0x0F, 0xC7, 0xEE, 0xD9, 0x1C, 0x4D, 0xB0, 0x2C, 0x14,
+	0x3D, 0xD5, 0x19, 0xB5, 0xA8, 0xBB, 0xDD, 0x08, 0x9C, 0x69, 0xF1, 0x9D, 0x7E, 0x77, 0xBB, 0xFF,
+	0x21, 0x43, 0x79, 0x6E, 0x09, 0xF3, 0x92, 0x77, 0xEB, 0x53, 0x8D, 0xF1, 0x66, 0xD1, 0xC0, 0x89,
+	0xB1, 0xF3, 0xF4, 0x31, 0xAA, 0x55, 0x1B, 0xEE, 0x9D, 0x59, 0xBF, 0xF8, 0x32, 0xD8, 0x94, 0xAD,
+	0x69, 0x30, 0x76, 0x60, 0x0F, 0x9C, 0xFB, 0xF0, 0xC7, 0xDB, 0xD4, 0x86, 0x02, 0x72, 0x49, 0xC4,
+	0x98, 0x9A, 0x5D, 0x92, 0x0F, 0x91, 0x2D, 0xF4, 0xC8, 0xF2, 0x99, 0x21, 0xD7, 0x64, 0xD3, 0x78,
+	0x0A, 0xF9, 0xD7, 0x08, 0x63, 0xB1, 0x1A, 0x85, 0xD4, 0x31, 0x98, 0x3B, 0x51, 0x5E, 0x6F, 0x78,
+	0xF5, 0xDC, 0x8E, 0x98, 0x21, 0x18, 0x41, 0x10, 0x13, 0xBF, 0xEB, 0x39, 0x15, 0x1A, 0x87, 0xA9,
+	0x78, 0xE6, 0xEC, 0x24, 0x27, 0xDC, 0x14, 0x1A, 0x7F, 0x20, 0x35, 0x6B, 0x10, 0x97, 0xCE, 0xA7,
+	0x44, 0x51, 0xF3, 0xF5, 0xD2, 0x77, 0x0D, 0x1B, 0x2A, 0xEB, 0x0C, 0x61, 0x22, 0x52, 0xEA, 0x8B,
+	0xD3, 0x24, 0xD7, 0x67, 0xA6, 0x2C, 0x3E, 0xAE, 0x0B, 0x5F, 0x80, 0x06, 0x7D, 0x04, 0x9E, 0x05,
+	0x32, 0xFE, 0xF8, 0x24, 0xF9, 0x07, 0xB8, 0xB6, 0x17, 0xBC, 0x24, 0x6A, 0xAC, 0x18, 0xE7, 0x7A,
+	0x14, 0x07, 0xC3, 0xCB, 0x15, 0xEB, 0x69, 0x84, 0xD4, 0x65, 0xAB, 0xD0, 0x09, 0x68, 0x32, 0x37,
+	0x3A, 0x04, 0xF7, 0x09, 0x88, 0x99, 0x80, 0x3A, 0x7E, 0xE4, 0x3F, 0x44, 0x5A, 0x59, 0xFB, 0x6B,
+	0x5A, 0x90, 0x05, 0x80, 0xC9, 0xDD, 0x21, 0x7D, 0x05, 0x5A, 0x19, 0xD1, 0xD5, 0x4B, 0xB9, 0x8D,
+	0x0D, 0x98, 0xE5, 0x26, 0xB3, 0xC6, 0x5A, 0x48, 0x57, 0x7A, 0x9B, 0x6A, 0x56, 0xA2, 0xBB, 0x93,
+	0x1D,
+};
+const size_t g_appkey_size = sizeof(g_appkey);
+
+sp_session *g_session;
+
+static int notify_events;
+static pthread_mutex_t notify_mutex;
+static pthread_cond_t notify_cond;
+
+
+static void connection_error(sp_session *session, sp_error error)
+{
+	
+}
+
+static void logged_in(sp_session *session, sp_error error)
+{
+	//add your testcode here 
+	
+}
+
+static void logged_out(sp_session *session)
+{
+	exit(0);
+}
+
+
+/**
+ * This callback is called for log messages.
+ *
+ * @sa sp_session_callbacks#log_message
+ */
+static void log_message(sp_session *session, const char *data)
+{
+	fprintf(stderr,"%s",data);
+}
+
+void notify_main_thread(sp_session *session)
+{
+	pthread_mutex_lock(&notify_mutex);
+	notify_events = 1;
+	pthread_cond_signal(&notify_cond);
+	pthread_mutex_unlock(&notify_mutex);
+}
+
+static sp_session_callbacks callbacks = {
+	&logged_in,
+	&logged_out,
+	NULL,
+	&connection_error,
+	NULL,
+	&notify_main_thread,
+	NULL,
+	NULL,
+	&log_message
+};
+
+int spotify_init(const char *username,const char *password)
+{
+	sp_session_config config;
+	sp_error error;
+	sp_session *session;
+	
+	// Always do this. It allows libspotify to check for
+	// header/library inconsistencies.
+	config.api_version = SPOTIFY_API_VERSION;
+
+	// The path of the directory to store the cache. This must be specified.
+	// Please read the documentation on preferred values.
+	config.cache_location = "tmp";
+
+	// The path of the directory to store the settings. 
+	// This must be specified.
+	// Please read the documentation on preferred values.
+	config.settings_location = "tmp";
+
+	// The key of the application. They are generated by Spotify,
+	// and are specific to each application using libspotify.
+	config.application_key = g_appkey;
+	config.application_key_size = g_appkey_size;
+
+	// This identifies the application using some
+	// free-text string [1, 255] characters.
+	config.user_agent = USER_AGENT;
+
+	// Register the callbacks.
+	config.callbacks = &callbacks;
+
+	error = sp_session_create(&config, &session);
+	if (SP_ERROR_OK != error) {
+		fprintf(stderr, "failed to create session: %s\n",
+		                sp_error_message(error));
+		return 2;
+	}
+
+	// Login using the credentials given on the command line.
+	error = sp_session_login(session, username, password, 0, 0);
+
+	if (SP_ERROR_OK != error) {
+		fprintf(stderr, "failed to login: %s\n",
+		                sp_error_message(error));
+		return 3;
+	}
+
+	g_session = session;
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int next_timeout = 0;
+	if(argc < 3) {
+		fprintf(stderr,"Usage: %s <username> <password>\n",argv[0]);
+	}
+	pthread_mutex_init(&notify_mutex, NULL);
+	pthread_cond_init(&notify_cond, NULL);
+
+	if(spotify_init(argv[1],argv[2]) != 0) {
+		fprintf(stderr,"Spotify failed to initialize\n");
+		exit(-1);
+	}
+	pthread_mutex_lock(&notify_mutex);
+	for (;;) {
+		if (next_timeout == 0) {
+			while(!notify_events)
+				pthread_cond_wait(&notify_cond, &notify_mutex);
+		} else {
+			struct timespec ts;
+
+	#if _POSIX_TIMERS > 0
+			clock_gettime(CLOCK_REALTIME, &ts);
+	#else
+			struct timeval tv;
+			gettimeofday(&tv, NULL);
+			TIMEVAL_TO_TIMESPEC(&tv, &ts);
+	#endif
+
+			ts.tv_sec += next_timeout / 1000;
+			ts.tv_nsec += (next_timeout % 1000) * 1000000;
+
+			while(!notify_events) {
+				if(pthread_cond_timedwait(&notify_cond, &notify_mutex, &ts))
+					break;
+			}
+		}
+
+		// Process libspotify events
+		notify_events = 0;
+		pthread_mutex_unlock(&notify_mutex);
+
+		do {
+			sp_session_process_events(g_session, &next_timeout);
+		} while (next_timeout == 0);
+
+		pthread_mutex_lock(&notify_mutex);
+	}
+	return 0;
+}
